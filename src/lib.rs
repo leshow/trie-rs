20eafbead1c021e66cfe116803d1ee3e6c5b534d
Evan Cameron <evan@vectorface.ca>
2016-11-18 11:38:34 -0500

    difficult

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,7 @@ use std::hash::Hash;
 use std::collections::hash_map::Entry::{Vacant, Occupied};
 use std::fmt::Debug;
 
-
-#[derive(Debug)]
+#[derive(Clone)]
 pub struct Trie<K, V>
     where V: Eq,
           K: Eq + Hash + Clone
@@ -23,35 +22,44 @@ impl<K, V> Trie<K, V>
             value: None,
         }
     }
+    pub fn with_value(self, v: Option<V>) -> Trie<K, V> {
+        Trie { value: v, ..self }
+    }
+}
+
+impl<K, V> Debug for Trie<K, V>
+    where V: Eq + Debug,
+          K: Eq + Hash + Clone + Debug
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f,
+               "Trie {{ \n \tchildren: {:?}, \n \tvalue: {:?} \n }}",
+               self.children,
+               self.value)
+    }
 }
 
 impl<K, V> Trie<K, V>
-    where V: Eq,
+    where V: Eq + Clone,
           K: Eq + Hash + Clone
 {
     pub fn insert<I>(&mut self, iter: I, value: V)
         where I: Iterator<Item = K>
     {
-        // let mut current_node = self;
-        // for c in iter.into_iter() {
-        //     let node = match current_node.children.entry(c) {
-        //         Vacant(slot) => slot.insert(Trie::new()),
-        //         Occupied(slot) => slot.into_mut(),
-        //     };
-        //     current_node = node;
+        // if let Some(first) = iter.nth(0) {
+        //     self.children.entry(first.clone()).or_insert(Trie::new()).insert(iter, value);
+        // } else {
+        //     self.value = Some(value);
         // }
-        let node = iter.fold(self, |current_node, c| {
-            match current_node.children.entry(c) {
-                Vacant(slot) => slot.insert(Trie::new()),
-                Occupied(slot) => slot.into_mut(),
+        let node = iter.fold(self, |cur_node, c| {
+            match cur_node.children.entry(c) {
+                Vacant(v) => v.insert(Trie::new()),
+                Occupied(v) => v.into_mut(),
             }
         });
         node.value = Some(value);
     }
     // fn remove<I>(&self, iter: I) -> bool {
-    //     unimplemented!();
-    // }
-    // fn contains<I: IntoIterator>(&self, iter: I) -> bool {
     //     unimplemented!();
     // }
 }
@@ -65,6 +73,9 @@ mod tests {
     fn test_insert() {
         let mut trie: Trie<char, u8> = Trie::new();
         trie.insert("first".chars(), 20);
+        trie.insert("fib".chars(), 30);
+        trie.insert("fibonacci".chars(), 30);
+        trie.insert("hello".chars(), 30);
         println!("{:?}", trie);
     }
 }
