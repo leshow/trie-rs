077ca12444a18e41d0845056453133d8865d6b64
Evan Cameron <evan@vectorface.ca>
2016-11-22 13:37:21 -0500

    fix intoiterator, pass str

--- src/lib.rs
+++ src/lib.rs
@@ -41,10 +41,10 @@ impl<K, V> Debug for Trie<K, V>
 
 impl<K, V> Trie<K, V>
     where V: Eq + Clone,
-          K: Eq + Hash + Clone
+          K: 'static + Eq + Hash + Clone
 {
-    pub fn insert_from_iter<I>(&mut self, iter: I, value: V)
-        where I: IntoIterator<Item = K>
+    pub fn insert<'a, I>(&mut self, iter: I, value: V)
+        where I: IntoIterator<Item = &'a K>
     {
         let mut node = self;
         for c in iter.into_iter() {
@@ -54,11 +54,11 @@ impl<K, V> Trie<K, V>
         node.value = Some(value);
     }
 
-    pub fn insert_fold<I>(&mut self, iter: I, value: V)
-        where I: IntoIterator<Item = K>
+    pub fn insert_fold<'a, I>(&mut self, iter: I, value: V)
+        where I: IntoIterator<Item = &'a K>
     {
         let node = iter.into_iter().fold(self, |cur_node, c| {
-            match cur_node.children.entry(c) {
+            match cur_node.children.entry(c.clone()) {
                 Vacant(v) => v.insert(Trie::new()),
                 Occupied(v) => v.into_mut(),
             }
@@ -66,25 +66,25 @@ impl<K, V> Trie<K, V>
         node.value = Some(value);
     }
 
-    pub fn insert_raw<I>(&mut self, iter: I, value: V)
-        where I: IntoIterator<Item = K>
+    pub fn insert_raw<'a, I>(&mut self, iter: I, value: V)
+        where I: IntoIterator<Item = &'a K>
     {
         let mut node = self;
         let mut raw_ptr: *mut Trie<K, V>; // node as *mut Trie<K, V>;
         for c in iter.into_iter() {
-            raw_ptr = node.children.entry(c).or_insert_with(Trie::new);
+            raw_ptr = node.children.entry(c.clone()).or_insert_with(Trie::new);
             unsafe {
                 node = &mut *raw_ptr;
             }
         }
         node.value = Some(value);
     }
-    pub fn contains_prefix<I>(&self, key: I) -> bool
-        where I: IntoIterator<Item = K>
+    pub fn contains_prefix<'a, I>(&self, key: I) -> bool
+        where I: IntoIterator<Item = &'a K>
     {
         let mut node = self;
         for c in key.into_iter() {
-            if !node.children.contains_key(&c) {
+            if !node.children.contains_key(c) {
                 return false;
             }
             let tmp = node;
@@ -118,27 +118,27 @@ impl<K, V> Trie<K, V>
 #[cfg(test)]
 mod tests {
     use super::Trie;
-    #[test]
-    fn test_insert() {
-        let mut trie = Trie::new();
-        let a = String::from("first");
-        trie.insert(&a, 20);
-        trie.insert("fib".chars(), 30);
-        trie.insert("fibonacci".chars(), 30);
-        trie.insert("hello".chars(), 30);
-    }
-    #[test]
-    fn test_vec_insert() {
-        let mut trie = Trie::new();
-        let b = vec![1, 2, 3, 4];
-        trie.insert(&b[..], 15);
+
+    fn build_trie() -> Trie<char, u8> {
+        let mut trie: Trie<char, u8> = Trie::new();
+        trie.insert(&['a', 'b'], 20);
+        trie.insert(&"first".chars().collect::<Vec<char>>(), 40);
+        trie.insert(&"fibonnaci".chars().collect::<Vec<char>>(), 40);
+        trie
     }
     #[test]
     fn test_contains() {
-        let mut trie: Trie<char, u8> = Trie::new();
-        trie.insert("first".chars(), 20);
-        assert!(trie.contains_prefix("f".chars()));
-        assert!(trie.contains_prefix("fi".chars()));
-        assert!(trie.contains_prefix("first".chars()));
+        let trie = build_trie();
+        assert!(trie.contains_prefix(&"f".chars().collect::<Vec<char>>()));
+        assert!(trie.contains_prefix(&"fi".chars().collect::<Vec<char>>()));
+        assert!(trie.contains_prefix(&"first".chars().collect::<Vec<char>>()));
     }
+    #[test]
+    fn test_raw_insert() {
+        let mut trie = Trie::new();
+        trie.insert_raw(&"first".chars().collect::<Vec<char>>(), 40);
+        trie.insert_raw(&"fibonnaci".chars().collect::<Vec<char>>(), 40);
+        assert!(trie.contains_prefix(&"fibonn".chars().collect::<Vec<char>>()));
+    }
+
 }
